47948eee35e2cc725abe5d8109d7b5ca67fdef68
root <root@Donnchadh>
2023-11-22 12:54:16 +0000

    loops over all files in fr94

--- Assignment2/src/main/java/org/querycrafters/App.java
+++ Assignment2/src/main/java/org/querycrafters/App.java
@@ -3,6 +3,7 @@ package org.querycrafters;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Stack;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -11,6 +12,7 @@ import org.apache.lucene.analysis.core.SimpleAnalyzer;
 
 import org.querycrafters.FBSIParser;
 import org.querycrafters.LATimesParser;
+import org.querycrafters.FR94Parser;
 
 public class App 
 {
@@ -47,6 +49,47 @@ public class App
                 System.out.println("Invalid analyzer type. Valid: StandardAnalyzer, SimpleAnalyzer, and EnglishAnalyzer.");
         }
 
+
+
+        // FR94Parser FRParser = new FR94Parser(analyzer, outputDir);
+        // File FRfolder = new File (System.getProperty("user.dir") + "/src/main/resources/Assignment Two/fr94/01");
+
+        // File[] FRfiles = FRfolder.listFiles(new FilenameFilter() {
+        //     @Override
+        //     public boolean accept(File dir, String name) {
+        //         return name.startsWith("fr");
+        //     }
+        // });
+        // for (File FRfile : FRfiles) {
+        //     FRParser.index(FRfile);
+        // }
+        // FRParser.shutdown();
+
+
+
+        FR94Parser FRParser = new FR94Parser(analyzer, outputDir);
+        File FRfolder = new File(System.getProperty("user.dir") + "/src/main/resources/Assignment Two/fr94");
+
+        Stack<File> directoryStack = new Stack<>();
+        directoryStack.push(FRfolder);
+
+        while (!directoryStack.isEmpty()) {
+            File currentDir = directoryStack.pop();
+            File[] files = currentDir.listFiles();
+
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        directoryStack.push(file); // Push subdirectories to stack for further processing
+                    } else if (file.getName().startsWith("fr")) {
+                        FRParser.index(file); // Process files
+                    }
+                }
+            }
+        }
+
+
+    
         System.out.println("Indexing Foreign Broadcast Information Service");
         FBSIParser FBSIParser = new FBSIParser(analyzer, outputDir);
         File FBSIfolder = new File (System.getProperty("user.dir") + "/src/main/resources/Assignment Two/fbis");
@@ -56,8 +99,8 @@ public class App
                 return name.startsWith("fb");
             }
         });
-        for (File file : FBSIfiles) {
-            FBSIParser.index(file);
+        for (File FBSIfile : FBSIfiles) {
+            FBSIParser.index(FBSIfile);
         }
         FBSIParser.shutdown();
 
@@ -70,8 +113,8 @@ public class App
                 return name.startsWith("la");
             }
         });
-        for (File file : LATimesfiles) {
-            LATimesParser.index(file);
+        for (File LATimesfile : LATimesfiles) {
+            LATimesParser.index(LATimesfile);
         }
         LATimesParser.shutdown();
     
